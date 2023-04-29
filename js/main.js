ac9b32fa3755a2ebf6ddc8bc76f2c166e10e4d79
Denowich <denowich@gmail.com>
2023-04-29 19:09:31 +0300

    completed burger & css, 860px

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector('.menu__btn"');
-const menu = document.querySelector('.menu__list"');
+const menuBtn = document.querySelector('.menu__btn');
+const menu = document.querySelector('.menu__list');
 
 menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('.menu__list--active');
+    menu.classList.toggle('menu__list--active');
 });
